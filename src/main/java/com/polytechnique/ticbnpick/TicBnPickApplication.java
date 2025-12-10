0a736e4bd946e6ca33b34c24be00a64538a6ee5d
KTBL16 <bernicetsafack@gmail.com>
2025-12-10 19:25:20 +0100

    Initial commit

---
+++ src/main/java/com/polytechnique/ticbnpick/TicBnPickApplication.java
@@ -0,0 +1,13 @@
+package com.polytechnique.ticbnpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicBnPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicBnPickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/polytechnique/ticbnpick/TicBnPickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytechnique.ticbnpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicBnPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
